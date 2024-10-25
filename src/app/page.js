826700a4f6f684c66d589e135271fc1de6d797c8
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2024-10-25 12:39:51 +0530

    route-done

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+import MarketingLandingView from 'src/sections/_marketing/view/marketing-landing-view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'The starting point for your next project',
+};
+
+export default function HomePage() {
+  return <MarketingLandingView />;
+}
+
+
---
+++ src/layouts/main/header.js
@@ -0,0 +1,114 @@
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import { useTheme } from '@mui/material/styles';
+
+import { useResponsive } from 'src/hooks/use-responsive';
+import { useOffSetTop } from 'src/hooks/use-off-set-top';
+
+import { bgBlur } from 'src/theme/css';
+
+import Logo from 'src/components/logo';
+import Label from 'src/components/label';
+
+import NavMobile from './nav/mobile';
+import NavDesktop from './nav/desktop';
+import { HEADER } from '../config-layout';
+import { navConfig } from './config-navigation';
+import HeaderShadow from '../common/header-shadow';
+
+// ----------------------------------------------------------------------
+
+export default function Header({ headerOnDark }) {
+  const theme = useTheme();
+
+  const offset = useOffSetTop();
+
+  const mdUp = useResponsive('up', 'md');
+
+  const renderContent = (
+    <>
+      <Box sx={{ lineHeight: 0, position: 'relative' }}>
+        <Logo />
+
+        <Link href="https://zone-docs.vercel.app/changelog" target="_blank" rel="noopener">
+          <Label
+            color="info"
+            sx={{
+              ml: 0.5,
+              px: 0.5,
+              top: -14,
+              left: 60,
+              height: 20,
+              fontSize: 11,
+              cursor: 'pointer',
+              position: 'absolute',
+            }}
+          >
+            v2.2.0
+          </Label>
+        </Link>
+      </Box>
+
+      {mdUp ? (
+        <Stack flexGrow={1} alignItems="flex-end" sx={{ height: 1 }}>
+          <NavDesktop data={navConfig} />
+        </Stack>
+      ) : (
+        <Box sx={{ flexGrow: 1 }} />
+      )}
+
+      {!mdUp && <NavMobile data={navConfig} />}
+    </>
+  );
+
+  return (
+    <AppBar>
+      <Toolbar
+        disableGutters
+        sx={{
+          height: {
+            xs: HEADER.H_MOBILE,
+            md: HEADER.H_DESKTOP,
+          },
+          transition: theme.transitions.create(['height', 'background-color'], {
+            easing: theme.transitions.easing.easeInOut,
+            duration: theme.transitions.duration.shorter,
+          }),
+          ...(headerOnDark && {
+            color: 'common.white',
+          }),
+          ...(offset && {
+            ...bgBlur({ color: theme.palette.background.default }),
+            color: 'text.primary',
+            height: {
+              md: HEADER.H_DESKTOP - 16,
+            },
+          }),
+        }}
+      >
+        <Container
+          sx={{
+            height: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {renderContent}
+        </Container>
+      </Toolbar>
+
+      {offset && <HeaderShadow />}
+    </AppBar>
+  );
+}
+
+Header.propTypes = {
+  headerOnDark: PropTypes.bool,
+};
